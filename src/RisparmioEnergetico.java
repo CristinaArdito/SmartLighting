1c4b757377e1e1c9e15c57a9364b8acebfba3369
Kri <cristina.ardito@studenti.unicam.it>
2018-01-10 17:55:59 +0100

    Definita classe RisparmioEnergetico

---
+++ src/RisparmioEnergetico.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class RisparmioEnergetico {
+	private RisparmioEnergetico statoPrecedente;
+	private Configurazione configurazione;
+	private Date data;
+	private int risparmio;
+	
+	public RisparmioEnergetico(RisparmioEnergetico statoPrecedente, Configurazione configurazione, Date data, int risparmio) {
+		super();
+		this.statoPrecedente = statoPrecedente;
+		this.configurazione = configurazione;
+		this.data = data;
+		this.risparmio = risparmio;
+	}
+	
+	public RisparmioEnergetico getStatoPrecedente() {
+		return statoPrecedente;
+	}
+	
+	public void setStatoPrecedente(RisparmioEnergetico statoPrecedente) {
+		this.statoPrecedente = statoPrecedente;
+	}
+	
+	public Configurazione getConfigurazione() {
+		return configurazione;
+	}
+	
+	public void setConfigurazione(Configurazione configurazione) {
+		this.configurazione = configurazione;
+	}
+	
+	public Date getData() {
+		return data;
+	}
+	
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	public int getRisparmio() {
+		return risparmio;
+	}
+	
+	public void setRisparmio(int risparmio) {
+		this.risparmio = risparmio;
+	}
+	
+	
+	
+}
