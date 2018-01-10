6a89a4efbcd75210f6ca40f975424e863a1e6328
Unknown <mattia.contigiani@gmail.com>
2018-01-10 17:41:25 +0100

    Modifica Cliente

--- src/Attori/Cliente.java
+++ src/Attori/Cliente.java
@@ -7,7 +7,47 @@ public class Cliente {
 	private String mail;
 	private String indirizzo;
 	
-	public boolean richiediSmartLighting() { 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public boolean richiediSmartLighting() {
 		return false;
 	};
 	
