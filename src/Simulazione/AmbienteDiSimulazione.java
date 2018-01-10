94ba5bace951ed8560546cf563ba76ee27c87196
Mattia <mattia.contigiani@gmail.com>
2018-01-10 18:04:59 +0100

    Fix AmbieneteDiSimulazione

--- src/Simulazione/AmbienteDiSimulazione.java
+++ src/Simulazione/AmbienteDiSimulazione.java
@@ -8,5 +8,29 @@ import toAssign.Sensore;
 public class AmbienteDiSimulazione {
 	private List<Sensore> sensori;
 	private List<Dispositivo> dispositivi;
-//	private List<Stanza> stanza;
+	private List<Stanza> stanze;
+	
+	public List<Sensore> getSensori() {
+		return sensori;
+	}
+	
+	public void setSensori(List<Sensore> sensori) {
+		this.sensori = sensori;
+	}
+	
+	public List<Dispositivo> getDispositivi() {
+		return dispositivi;
+	}
+	
+	public void setDispositivi(List<Dispositivo> dispositivi) {
+		this.dispositivi = dispositivi;
+	}
+	
+	public List<Stanza> getStanze() {
+		return stanze;
+	}
+	
+	public void setStanze(List<Stanza> stanze) {
+		this.stanze = stanze;
+	}
 }
