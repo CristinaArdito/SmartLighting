dbdbedb2fc892ff72003cd3f21eff3f6d216bb25
Unknown <mattia.contigiani@gmail.com>
2018-01-10 17:51:32 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/Sistema.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Sistema {
+	private List<Sensore> sensori;
+//	private Configurazione configurazione;
+//	private RisparmioEnergetico risparmio;
+	
+	public Sistema() {
+		
+	};
+	
+	public void setSensori() {
+		
+	};
+	
+	public List<Sensore> getSensori(){
+		return sensori;
+	}
+}
