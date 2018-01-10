c9f8306710b26bc7cef0fe343f0f5f686427fba0
Unknown <mattia.contigiani@gmail.com>
2018-01-10 17:55:23 +0100

    Definizione completa Sistema

--- src/Sistema.java
+++ src/Sistema.java
@@ -2,18 +2,58 @@ import java.util.List;
 
 public class Sistema {
 	private List<Sensore> sensori;
-//	private Configurazione configurazione;
+	private Configurazione configurazione;
 //	private RisparmioEnergetico risparmio;
 	
 	public Sistema() {
 		
-	};
-	
-	public void setSensori() {
-		
-	};
-	
-	public List<Sensore> getSensori(){
+	}
+
+	public List<Sensore> getSensori() {
 		return sensori;
+	}
+
+	public void setSensori(List<Sensore> sensori) {
+		this.sensori = sensori;
+	}
+
+	public Configurazione getConfigurazione() {
+		return configurazione;
+	}
+
+	public void setConfigurazione(Configurazione configurazione) {
+		this.configurazione = configurazione;
+	};
+	
+	public void lightOn() {
+		
+	}
+	
+	public void lightOff() {
+		
+	}
+	
+	public void Control() {
+	
+	}
+	
+	public void deviceOn() {
+	
+	}
+	
+	public void deviceOff() {
+		
+	}
+	
+	public void deviceStandBy() {
+		
+	}
+	
+	public void setConsumo() {
+		
+	}
+	
+	public void getConsumo() {
+		
 	}
 }
