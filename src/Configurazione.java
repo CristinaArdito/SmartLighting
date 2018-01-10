8c8877e61a7368fa642f517c7972ed197f9668d2
Kri <cristina.ardito@studenti.unicam.it>
2018-01-10 17:50:13 +0100

    Definita classe Configurazione

---
+++ src/Configurazione.java
@@ -0,0 +1,36 @@
+
+public class Configurazione {
+	private Dispositivo dispositivi;
+	private Dispositivo eccezioni;
+	
+	public Configurazione(Dispositivo dispositivi, Dispositivo eccezioni) {
+		super();
+		this.dispositivi = dispositivi;
+		this.eccezioni = eccezioni;
+	}
+
+	public Dispositivo getDispositivi() {
+		return dispositivi;
+	}
+
+	public void setDispositivi(Dispositivo dispositivi) {
+		this.dispositivi = dispositivi;
+	}
+
+	public Dispositivo getEccezioni() {
+		return eccezioni;
+	}
+
+	public void setEccezioni(Dispositivo eccezioni) {
+		this.eccezioni = eccezioni;
+	}
+	
+	public void setConfigurazione(Dispositivo dispositivi, Dispositivo eccezioni) {
+		this.dispositivi = dispositivi;
+		this.eccezioni = eccezioni;
+	}
+	
+	public Configurazione getConfigurazione() {
+		return this;
+	}
+}
