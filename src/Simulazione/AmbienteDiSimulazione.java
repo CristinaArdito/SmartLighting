ee64e59e1ca11e8509fe7c9e3274daec98f59484
Mattia <mattia.contigiani@gmail.com>
2018-01-19 12:57:03 +0100

    Modifica parametri simulazione

--- src/Simulazione/AmbienteDiSimulazione.java
+++ src/Simulazione/AmbienteDiSimulazione.java
@@ -26,7 +26,7 @@ public class AmbienteDiSimulazione {
 	
 	public Dispositivo generaDispositivo() {
 		String[] nomi = {"Televisore", "Computer", "Radio", "Forno", "Luci", "Stereo", "Condizionatore"};
-		Dispositivo temporaneo = new Dispositivo(nomi[(int) Math.random()], -1, idCorrente);
+		Dispositivo temporaneo = new Dispositivo(nomi[(int) Math.random()*7], -1, idCorrente);
 		idCorrente++;
 		return temporaneo;
 	};
@@ -39,14 +39,19 @@ public class AmbienteDiSimulazione {
 		AmbienteDiSimulazione simulazione = new AmbienteDiSimulazione();
 		List<Stanza> stanze = new ArrayList<Stanza>();
 		List<Dispositivo> dispositivi = new ArrayList<Dispositivo>();
+		List<Dispositivo> clone;
 		int random;
 		
 		for(int j=0; j<5; j++) {
-			random = (int) Math.random()*4;
+			random = (int) (Math.random()*5)+1;
+			System.out.println(random);
 				for(int i=0;i<random;i++) {
 					dispositivi.add(simulazione.generaDispositivo());
 				}
-			stanze.add(new Stanza(j,"Stanza "+j, dispositivi, simulazione.generaSensore()));
+			clone = new ArrayList<Dispositivo>();
+			clone.addAll(dispositivi);
+			stanze.add(new Stanza(j,"Stanza "+j, clone, simulazione.generaSensore()));
+			dispositivi.clear();
 		}
 		return stanze;
 	}
