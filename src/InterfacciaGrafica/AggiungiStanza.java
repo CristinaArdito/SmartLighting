e4cff5e83f4b23c30cde72fddc92b41cb86cc42b
Kri <cristina.ardito@studenti.unicam.it>
2018-01-19 16:00:48 +0100

    Interfaccia grafica per l'aggiunta della stanza e del singolo dispositivo (wip)

--- src/InterfacciaGrafica/AggiungiStanza.java
+++ src/InterfacciaGrafica/AggiungiStanza.java
@@ -1,10 +1,19 @@
 package InterfacciaGrafica;
 
 import javax.swing.JDialog;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
 
 public class AggiungiStanza extends JDialog {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	public static void main(String[] args) {
 		try {
@@ -19,6 +28,52 @@ public class AggiungiStanza extends JDialog {
 	public AggiungiStanza() {
 		setBounds(100, 100, 500, 750);
 		getContentPane().setLayout(null);
+		
+		JButton button = new JButton("Conferma");
+		button.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		button.setBounds(156, 657, 179, 43);
+		getContentPane().add(button);
+		
+		textField = new JTextField();
+		textField.setBounds(74, 81, 171, 30);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JRadioButton rdbtnTv = new JRadioButton("Tv");
+		rdbtnTv.setBounds(10, 177, 109, 23);
+		getContentPane().add(rdbtnTv);
+		
+		JRadioButton rdbtnComputer = new JRadioButton("Computer");
+		rdbtnComputer.setBounds(10, 203, 109, 23);
+		getContentPane().add(rdbtnComputer);
+		
+		JRadioButton rdbtnRadio = new JRadioButton("Radio");
+		rdbtnRadio.setBounds(10, 232, 109, 23);
+		getContentPane().add(rdbtnRadio);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(174, 389, 86, 30);
+		getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNumeroDiLuci = new JLabel("Numero di luci presenti:");
+		lblNumeroDiLuci.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNumeroDiLuci.setBounds(10, 387, 200, 30);
+		getContentPane().add(lblNumeroDiLuci);
+		
+		JLabel lblListaDeiDispositivi = new JLabel("Lista dei dispositivi presenti");
+		lblListaDeiDispositivi.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblListaDeiDispositivi.setBounds(10, 134, 200, 36);
+		getContentPane().add(lblListaDeiDispositivi);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNome.setBounds(10, 79, 69, 30);
+		getContentPane().add(lblNome);
+		
+		JLabel lblAggiungiUnaStanza = new JLabel("Aggiungi una stanza");
+		lblAggiungiUnaStanza.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblAggiungiUnaStanza.setBounds(171, 11, 153, 30);
+		getContentPane().add(lblAggiungiUnaStanza);
 	}
-
 }
--- src/InterfacciaGrafica/ModificaSingoloDispositivo.java
+++ src/InterfacciaGrafica/ModificaSingoloDispositivo.java
@@ -9,26 +9,58 @@ import javax.swing.SwingConstants;
 import toAssign.Configurazione;
 import toAssign.Dispositivo;
 import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class ModificaSingoloDispositivo extends JDialog {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	public ModificaSingoloDispositivo(String nomeDispositivo, int idDispositivo, Configurazione config) {
 		getContentPane().setLayout(null);
-		setBounds(100, 100, 500, 750);
 		
-		JLabel label = new JLabel(nomeDispositivo+" - ID:"+idDispositivo);
-		label.setFont(new Font(label.getFont().getFontName(),label.getFont().getStyle(),22));
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setBounds(10, 11, 414, 35);
-		getContentPane().add(label);
+		JLabel lblModificaDispositivo = new JLabel("Modifica dispositivo");
+		lblModificaDispositivo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblModificaDispositivo.setBounds(158, 11, 217, 27);
+		getContentPane().add(lblModificaDispositivo);
+		
+		JLabel lblInserisciLaTipologia = new JLabel("Inserisci la tipologia di dispositivo");
+		lblInserisciLaTipologia.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblInserisciLaTipologia.setBounds(10, 82, 217, 27);
+		getContentPane().add(lblInserisciLaTipologia);
+		
+		textField = new JTextField();
+		textField.setBounds(237, 81, 197, 32);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblInserisciIlConsumo = new JLabel("Inserisci il consumo");
+		lblInserisciIlConsumo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblInserisciIlConsumo.setBounds(10, 140, 217, 27);
+		getContentPane().add(lblInserisciIlConsumo);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(158, 145, 104, 20);
+		getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblWatt = new JLabel("watt");
+		lblWatt.setBounds(272, 148, 46, 14);
+		getContentPane().add(lblWatt);
+		
+		JButton button = new JButton("Conferma");
+		button.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		button.setBounds(158, 616, 179, 43);
+		getContentPane().add(button);
+		setBounds(100, 100, 500, 750);
 		
 		Dispositivo dispositivo = config.getDispositivo(idDispositivo);
 		if(dispositivo != null) {
 		JLabel accendibile = new JLabel("Il dispositivo può essere acceso");
 		accendibile.setBounds(10, 99, 290, 64);
-		accendibile.setFont(new Font(label.getFont().getFontName(),label.getFont().getStyle(),18));
+		accendibile.setFont(new Font(lblModificaDispositivo.getFont().getFontName(),lblModificaDispositivo.getFont().getStyle(),18));
 		getContentPane().add(accendibile);	
 		
 		JCheckBox checkAccendibile = new JCheckBox();
@@ -38,7 +70,7 @@ public class ModificaSingoloDispositivo extends JDialog {
 		
 		JLabel standBy = new JLabel("Il dispositivo può essere messo in standby");
 		standBy.setBounds(10, 184, 344, 64);
-		standBy.setFont(new Font(label.getFont().getFontName(),label.getFont().getStyle(),18));
+		standBy.setFont(new Font(lblModificaDispositivo.getFont().getFontName(),lblModificaDispositivo.getFont().getStyle(),18));
 		getContentPane().add(standBy);	
 		
 		JCheckBox checkStandBy = new JCheckBox();
@@ -48,7 +80,7 @@ public class ModificaSingoloDispositivo extends JDialog {
 		
 		JLabel spegnibile = new JLabel("Il dispositivo può essere spento");
 		spegnibile.setBounds(10, 271, 290, 64);
-		spegnibile.setFont(new Font(label.getFont().getFontName(),label.getFont().getStyle(),18));
+		spegnibile.setFont(new Font(lblModificaDispositivo.getFont().getFontName(),lblModificaDispositivo.getFont().getStyle(),18));
 		getContentPane().add(spegnibile);	
 		
 		JCheckBox checkSpegnibile = new JCheckBox();
