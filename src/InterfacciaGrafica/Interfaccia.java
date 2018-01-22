61217693005bbb417ccdd464a6c967c197e790c5
Kri <cristina.ardito@studenti.unicam.it>
2018-01-22 17:11:37 +0100

    Adattati i metodi readStanzaFromFile e writeStanzaOnFile in modo che salvino e leggano tutte le stanze presenti nella casa

--- src/InterfacciaGrafica/Interfaccia.java
+++ src/InterfacciaGrafica/Interfaccia.java
@@ -3,6 +3,7 @@ package InterfacciaGrafica;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -13,6 +14,23 @@ import javax.swing.border.EmptyBorder;
 
 import Simulazione.AmbienteDiSimulazione;
 import toAssign.Configurazione;
+import toAssign.Dispositivo;
+import toAssign.Sensore;
+import toAssign.Sistema;
+import toAssign.Stanza;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.awt.event.ActionEvent;
+
+
 import toAssign.Sistema;
 import toAssign.Stanza;
 
@@ -24,9 +42,31 @@ public class Interfaccia extends JFrame {
 	private Configurazione config = new Configurazione();
 	private static List<Integer> sensori = AmbienteDiSimulazione.ottieniSensori();
 
-	public Interfaccia() {
+	public Interfaccia() throws FileNotFoundException, MalformedURLException {
 		
 		stanze = new ArrayList<Stanza>();
+		stanze.add(new Stanza(0, "Cucina", ottiniDispositivi(), new Sensore(1)));
+		stanze.add(new Stanza(1, "Bagno", ottiniDispositivi(), new Sensore(-1)));
+		File file = new File("Stanze.txt");
+		writeStanzaOnFile(file);
+		List<Stanza> list1= readStanzaFromFile(file);
+		Iterator<Stanza> j = list1.iterator();
+		Stanza s1;
+		while(j.hasNext()) {
+			s1 = j.next();
+			System.out.println(s1.getNome());
+			System.out.println(s1.getCodice());
+			System.out.println("dispositivi:");
+			List<Dispositivo> list = s1.getDispositivi();
+			Dispositivo d;
+			Iterator<Dispositivo> i = list.iterator();
+			while(i.hasNext()) {
+				d = i.next();
+				System.out.println(d.getTipo() + d.getCodice() + d.getId() + d.puòEssereAcceso() + d.puòEssereSpento() + d.puòEssereMessoInStandby() + d.getConsumo());
+			}
+			System.out.println(s1.getSensore().getCodice());
+		}
+		
 //		importaConfigurazione();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -77,5 +117,124 @@ public class Interfaccia extends JFrame {
 	
 	public static List<Integer> getSensori(){
 		return sensori;
+	}
+	
+	/**
+	 * Scrittura della stanza su file
+	 * @param file
+	 * @throws FileNotFoundException
+	 * @throws MalformedURLException
+	 */
+	public void writeStanzaOnFile(File file) throws FileNotFoundException, MalformedURLException {
+		Iterator<Stanza> j = stanze.iterator();
+		Stanza s;
+		while(j.hasNext()) {
+			s = j.next();
+			List<Dispositivo> dispositivi = s.getDispositivi();
+			Iterator<Dispositivo> i = dispositivi.iterator();
+			Dispositivo d;
+			
+		    try {
+			    FileOutputStream output = new FileOutputStream(file, true);
+			    PrintStream write = new PrintStream(output); 
+			    
+			    write.println("{");
+			    write.println("Nome: " + s.getNome() + " ,");
+			    write.println("Codice: " + s.getCodice() + " ,");
+			    write.print("Lista Dispositivi: ");
+			    write.println();
+			    while(i.hasNext()) {
+			    	d = i.next();
+			    	write.println("{");
+			    	write.println("Tipo: " + d.getTipo() + " ,");
+			    	write.println("Codice: " + d.getCodice() + " ,");
+			    	write.println("Id: " + d.getId() + " ,");
+			    	write.println("PuòEssereAcceso: " + d.puòEssereAcceso() + " ,");
+			    	write.println("PuòEssereSpento: " + d.puòEssereSpento() + " ,");
+			    	write.println("PuòEssereMessoInStandby: " + d.puòEssereMessoInStandby() + " ,");
+			    	write.println("Consumo: " + d.getConsumo());
+			    	write.println("},");
+			    }
+			    write.println("Sensore: " + s.getSensore().getCodice());
+			    write.print("};");		    
+			    write.close();
+		    }
+		    catch (IOException e) {
+			    System.out.println(e.getMessage());
+		    }
+		    catch (Exception e) {
+			    System.out.println(e.getMessage());		   
+		    } 
+		}
+	}  
+	
+	public List<Stanza> readStanzaFromFile(File file) throws FileNotFoundException{
+    	URI uri = file.toURI();		/* Restituisce un URI assoluto con uno schema uguale al file */
+    	byte[] bytes = null;		/* Creo un array di bytes */
+    	List<Dispositivo> list = new ArrayList<Dispositivo>();
+    	int codice = 0, id = 0, codicestanza = 0;
+    	boolean on = false, off = false, stand = false;
+    	double consumo = 0;
+    	String tipo = new String();
+    	String nome = new String();
+    	Stanza app = null;
+    	Sensore s;
+    	List<Stanza> liststanze = new ArrayList<Stanza>();
+        try {
+        	/* Leggo tutti i byte del file */
+            bytes = java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(uri));
+        }catch(IOException e) { 
+        	e.printStackTrace(); 
+        }
+        /* Trasformo l'array di byte in una stringa */
+        String stanza =  new String(bytes);
+        String[] lines = stanza.split(System.getProperty("line.separator"));       
+        for(String line : lines) {
+        	String[] words = line.split("\\s+");
+        	if(line.contains("Nome") == true) {
+        		nome = words[1];
+        	}
+        	else if(line.contains("Codice") == true) {
+        		codicestanza = Integer.parseInt(words[1]);
+        	}
+        	else if(line.contains("Tipo") == true) {
+        		tipo = words[1];
+        	}
+        	else if(line.contains("Codice") == true) {
+        		codice = Integer.parseInt(words[1]);
+        	}
+        	else if(line.contains("Id") == true) {
+        		id = Integer.parseInt(words[1]);
+        	}
+        	else if(line.contains("PuòEssereAcceso") == true) {
+        		on = Boolean.parseBoolean(words[1]);
+        	}
+        	else if(line.contains("PuòEssereSpento") == true) {
+        		off = Boolean.parseBoolean(words[1]);
+        	}
+        	else if(line.contains("PuòEssereMessoInStandby ") == true) {
+        		stand = Boolean.parseBoolean(words[1]);
+        	}
+        	else if(line.contains("Consumo") == true) {
+        		consumo = Double.parseDouble(words[1]);
+        		list.add(new Dispositivo(tipo, codice, id, consumo, on, off, stand));
+        	}
+        	else if(line.contains("Sensore") == true) {
+        		s = new Sensore(Integer.parseInt(words[1]));
+        		liststanze.add(new Stanza(codicestanza, nome, list, s));
+        		list.clear();
+        	}
+        }
+        return liststanze;
+	}
+	
+	public static List<Dispositivo> ottiniDispositivi() {
+		List<Dispositivo> listaDispositivi = new ArrayList<Dispositivo>();
+		listaDispositivi.add(new Dispositivo("Tv", 1, 00, 120.00, true, false, false));
+		listaDispositivi.add(new Dispositivo("Minitor", 1, 01, 90.00, true, false, false));
+		listaDispositivi.add(new Dispositivo("Computer", 0, 02, 20.00, true, false, false));
+		listaDispositivi.add(new Dispositivo("Lavatrice", -1, 03, 220.00, true, false, false));
+		listaDispositivi.add(new Dispositivo("Radio", 0, 04, 10.00, true, false, false));
+		return listaDispositivi;
 	}
 }
