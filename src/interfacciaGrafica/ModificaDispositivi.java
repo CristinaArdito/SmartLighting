bd782919e79670e21afed2b52f8094d842a0f22e
Kri <cristina.ardito@studenti.unicam.it>
2018-01-26 17:39:21 +0100

    Revert "Revert "Restyle generale applicazione""
    This reverts commit 58c6201b9458de6c5a5656927f64fd017da86ee7.

--- src/interfacciaGrafica/ModificaDispositivi.java
+++ src/interfacciaGrafica/ModificaDispositivi.java
@@ -2,8 +2,10 @@ package interfacciaGrafica;
 
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JList;
@@ -15,40 +17,60 @@ import sistema.Stanza;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
 
 public class ModificaDispositivi extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private JList<String> listaDispositivi;
 	DefaultListModel<String> modelloDispositivi;
+
 	
 
 	public ModificaDispositivi(int idStanza, List<Stanza> stanze, Configurazione config, List<Integer> listaIdDispositivi) {
+		getContentPane().setBackground(new Color(236,248,250));
 		setBounds(100, 100, 500, 750);
 		getContentPane().setLayout(null);
 		setUndecorated(true);
 		getRootPane().setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
 		
+		ImageIcon img = new ImageIcon("icon.png");
+		this.setIconImage(img.getImage());
+		BufferedImage myPicture = null;
+		try {
+			myPicture = ImageIO.read(new File("sls_logo.png"));
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+		picLabel.setBounds(27, 11, 432, 86);
+		getContentPane().add(picLabel);
+		
 		JButton btnConferma = new JButton("Conferma");
-		btnConferma.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnConferma.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnConferma.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		btnConferma.setBounds(20, 629, 190, 52);
+		btnConferma.setBounds(27, 654, 191, 49);
 		getContentPane().add(btnConferma);
 		JButton btnAggiungiDispositivo = new JButton("Aggiungi");
-		btnAggiungiDispositivo.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnAggiungiDispositivo.setBounds(284, 629, 190, 52);
+		btnAggiungiDispositivo.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnAggiungiDispositivo.setBounds(276, 654, 191, 49);
 		getContentPane().add(btnAggiungiDispositivo);
 		
 		listaDispositivi = new JList<String>();
-		listaDispositivi.setBounds(10, 52, 462, 566);
+		listaDispositivi.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		listaDispositivi.setBorder(new LineBorder(new Color(0, 0, 0)));
+		listaDispositivi.setBounds(10, 133, 480, 487);
 		getContentPane().add(listaDispositivi);
 		modelloDispositivi = new DefaultListModel<String>();
 		
@@ -72,13 +94,14 @@ public class ModificaDispositivi extends JDialog {
 		listaDispositivi.setModel(modelloDispositivi);
 		
 		JLabel lblAggiungiDispositivo = new JLabel("Aggiungi Dispositivo");
-		lblAggiungiDispositivo.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblAggiungiDispositivo.setBounds(161, 11, 180, 26);
+		lblAggiungiDispositivo.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblAggiungiDispositivo.setBounds(160, 96, 191, 26);
 		getContentPane().add(lblAggiungiDispositivo);
 		
 		btnAggiungiDispositivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				AggiungiDispositivo aggiungiDispositivo = new AggiungiDispositivo(false, listaIdDispositivi);
+				AggiungiDispositivo aggiungiDispositivo;
+				aggiungiDispositivo = new AggiungiDispositivo(false, listaIdDispositivi);
 				aggiungiDispositivo.setVisible(true);
 				aggiungiDispositivo.addRoomListener(new ActionListener() {
 					
