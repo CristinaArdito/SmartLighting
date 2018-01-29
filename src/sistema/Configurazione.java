53d2254d26f7061c59bbf0bb33fe7d4f4590ced9
Kri <cristina.ardito@studenti.unicam.it>
2018-01-29 16:47:34 +0100

    Terminata configurazione, aggiunto package diagram

--- src/sistema/Configurazione.java
+++ src/sistema/Configurazione.java
@@ -5,48 +5,86 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Configurazione {
+	/*
+	 * Lista dispositivi 
+	 */
 	private List<Dispositivo> dispositivi;
 	
+	/**
+	 * Costruttore della Configurazione con parametri
+	 * @param dispositivi
+	 */
 	public Configurazione(List<Dispositivo> dispositivi) {
 		super();
 		this.dispositivi = new ArrayList<Dispositivo>();
 		this.dispositivi.addAll(dispositivi);
 	}
 	
+	/**
+	 * Costruttore Configurazione senza parametri
+	 */
 	public Configurazione() {
 		super();
 		this.dispositivi = new ArrayList<Dispositivo>();
 	}
 
+	/**
+	 * Ritorna la lista dei dispositivi della configurazione
+	 * @return		dispositivi
+	 */
 	public List<Dispositivo> getDispositivi() {
 		return dispositivi;
 	}
 
+	/**
+	 * Inserisce i dispositivi delle stanze della casa
+	 * @param stanze		stanze della casa
+	 */
 	public void setDispositivi(List<Stanza> stanze) {
+		// Creo un iteratore
 		Iterator<Stanza> i = stanze.iterator();
+		// Dispositivo di appoggio
 		Dispositivo d;
+		// Stanza di appoggio
 		Stanza stanza;
 		
+		// Itero le stanze
 		while(i.hasNext()) {
 			stanza = i.next();
+			// Ottengo i vari dispositivi della stanza
 			List<Dispositivo> listdisp = stanza.getDispositivi();
+			// Itero i dispositivi
 			Iterator<Dispositivo> j = listdisp.iterator();
 			while(j.hasNext()) {
 				d = j.next();
+				// Aggiungo i dispositivi alla lista
 				dispositivi.add(d);
 			}
 		}
 		
 	}
 	
-	public void setConfigurazione(List<Dispositivo> dispositivi, List<Dispositivo> eccezioni) {
+	/**
+	 * Inserisce la configurazione
+	 * @param dispositivi
+	 */
+	public void setConfigurazione(List<Dispositivo> dispositivi) {
 		this.dispositivi = dispositivi;
 	}
 	
+	/**
+	 * Ritorna la configurazione corrente
+	 * @return		Configurazione
+	 */
 	public Configurazione getConfigurazione() {
 		return this;
 	}
 	
+	/**
+	 * Ritorna il dispositivo in base al suo id
+	 * @param idDispositivo
+	 * @return		dispositivo
+	 */
 	public Dispositivo getDispositivo(int idDispositivo) {
 		for (Dispositivo dispositivo : dispositivi) {
 			if(dispositivo.getId() == idDispositivo) {
@@ -56,15 +94,27 @@ public class Configurazione {
 		return null;
 	}
 	
+	/**
+	 * Aggiunge un dispositivo alla lista
+	 * @param dispositivo
+	 * @return
+	 */
 	public boolean addDispositivo(Dispositivo dispositivo) {
 		try {
-		dispositivi.add(dispositivo);
-		return true;
+			dispositivi.add(dispositivo);
+			return true;
 		}catch(Exception e) {
 			return false;
 		}
 	}
 	
+	/**
+	 * Configura un dispositivo	
+	 * @param id			id del dispositivo
+	 * @param stato			stato del dispositivo
+	 * @param attributo		attributo
+	 * @return		true/false
+	 */
 	public boolean ConfiguraDispositivo(int id, int stato, boolean attributo) {
 		for (Dispositivo dispositivo : dispositivi) {
 			 if(dispositivo.getId() == id) {
